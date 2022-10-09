8e648ea78b6017b14c44ddb1b0e214211e61d192
Tiloye <papiski49@gmail.com>
2022-10-09 18:19:02 +0100

    updated Gradio_App

---
+++ Gradio_App/app.py
@@ -0,0 +1,61 @@
+import gradio as gr
+from metrics import show_metrics, estimate_costs
+
+# buid app UI
+with gr.Blocks(css=".gradio-container {/*...*/ align-items: center}", title="Precision-Recall Trade-off for Churn Prediction Model") as demo:
+
+    with gr.Row():
+        pr_threshold = gr.Slider(minimum=0.0, maximum=1.0, value=0.5)
+    with gr.Row():
+        with gr.Column(min_width=500):
+            pr_confusion_matrix = gr.Plot(label="True Value vs Predicted Value")
+        with gr.Column(scale=2):
+            with gr.Row():
+                with gr.Column():
+                    accuracy = gr.Number(label="Accuracy Score")
+                    precision = gr.Number(label="Precision Score")
+                    recall = gr.Number(label="Recall Score")
+                with gr.Column():
+                    crc = gr.Number(value=50, label="Customer Retention Cost (€)")
+                    cac = gr.Number(value=200, label="Customer Acquisiton Cost(€)")
+            with gr.Row():
+                with gr.Column():
+                    total_crc = gr.Number(label="Total Customer Retention Cost(€)")
+                    total_cac = gr.Number(label="Total Customer Aquisition Cost(€)")
+                with gr.Column():
+                    total_amount = gr.Number(label="Total Amount Spent(€)")
+                    amount_saved = gr.Number(label="Amount Saved(€)")
+
+    demo.load(
+        fn=show_metrics,
+        inputs=[pr_threshold],
+        outputs=[pr_confusion_matrix, accuracy, precision, recall]
+    )
+    demo.load(
+        fn=estimate_costs,
+        inputs=[pr_threshold, crc, cac],
+        outputs=[total_crc, total_cac, total_amount, amount_saved]
+    )
+    pr_threshold.change(
+        fn=show_metrics,
+        inputs=[pr_threshold],
+        outputs=[pr_confusion_matrix, accuracy, precision, recall]   
+    )
+    pr_threshold.change(
+        fn=estimate_costs,
+        inputs=[pr_threshold, crc, cac],
+        outputs=[total_crc, total_cac, total_amount, amount_saved]
+    )
+    crc.change(
+        fn=estimate_costs,
+        inputs=[pr_threshold, crc, cac],
+        outputs=[total_crc, total_cac, total_amount, amount_saved]
+    )
+    cac.change(
+        fn=estimate_costs,
+        inputs=[pr_threshold, crc, cac],
+        outputs=[total_crc, total_cac, total_amount, amount_saved]
+    )
+
+if __name__ == "__main__":
+    demo.launch()
---
+++ Gradio_App/metrics.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib as jb
+import plotly.express as px
+import pathlib
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, precision_score, recall_score
+
+# load data
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("data").resolve()
+test_data = pd.read_csv(DATA_PATH.joinpath("test_data.csv"))
+
+# split data
+target = "Exited"
+X = test_data.drop(columns=["RowNumber", "Exited"])
+y = test_data[target]
+
+# load model
+MODEL_PATH = PATH.joinpath("model").resolve()
+model = jb.load(MODEL_PATH.joinpath("model.pkl"))
+
+# get prediction probabilities
+prediction_probabilites = model.predict_proba(X)[:,1]
+
+# define metrics function
+def show_metrics(probability_threshold):
+    """
+    parameters
+    ----------
+    probability_threshold: float
+        probability threshold for model prediction
+    
+    return
+    -------
+    figure: ConfusionMatrix
+        Confusion matrix showing True values vs Predicted values
+    evaluation metrics: float
+        Accuracy, Precision, and Recall scores
+    """
+
+    # get predictions
+    predictions = (prediction_probabilites >= probability_threshold).astype(int)
+
+    # get confusion matrix
+    matrix_array = confusion_matrix(y, predictions)
+
+    # plot confusion matrix
+    fig = px.imshow(
+        matrix_array,
+        labels=dict(x="Predicted Value", y="True Value"),
+        x = ["False", "True"],
+        y = ["False", "True"],
+        title="Confusion Matrix",
+        text_auto=True,
+        aspect="auto",
+        color_continuous_scale=px.colors.sequential.gray
+        )
+    # fig.update_xaxes(side="bottom")
+    fig.update_layout(title_x=0.5, coloraxis_showscale=False)
+
+    # get metrics
+    accuracy = accuracy_score(y, predictions).round(2)
+    precision = precision_score(y, predictions).round(2)
+    recall = recall_score(y, predictions).round(2)
+
+    return fig, accuracy, precision, recall
+
+def estimate_costs(probability_threshold, crc, cac):
+    """
+    parameters
+    ----------
+    probability_threshold: float
+        probability threshold for model prediction
+    crc: int
+        Customer Retention Cost
+    cac: int
+        Customer Aquisition Cost
+
+    return
+    -------
+    retention_cost: int
+        Total amount spent on customer retention
+    aqusition_cost: int
+        Total amount sent on customer acqusition
+    total_amount: int
+        Total cost of customer retention and aquisition
+    amount_saved: int
+        amount saved due to model precision
+    """
+
+    # get predictions
+    predictions = (prediction_probabilites >= probability_threshold).astype(int)
+
+    # get confusion matrix
+    matrix_array = confusion_matrix(y, predictions)
+
+    # get costs
+    retention_cost = matrix_array.sum(axis=0)[1] * crc # crc cost with model
+    aquisition_cost = matrix_array[1][0] * cac # cac with model
+    total_amount = retention_cost + aquisition_cost # total_amount amount spent on customer aquisition and retention
+    amount_saved = (matrix_array.sum(axis=1)[1] * cac) - total_amount
+
+    return retention_cost, aquisition_cost, total_amount, amount_saved
